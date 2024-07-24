725a7ac630cb1ae03158c8becb0e52bafeecdb18
thanaphat-tim12 <thnphathrtasay044@gmail.com>
2024-07-24 23:30:57 +0700

    Add 'Notify'in quasar.comf.js

--- quasar.config.js
+++ quasar.config.js
@@ -84,21 +84,12 @@ module.exports = configure(function (ctx) {
     },
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-framework
-    framework: {
-      config: {},
-
-      // iconSet: 'material-icons', // Quasar icon set
-      // lang: 'en-US', // Quasar language pack
-
-      // For special cases outside of where the auto-import strategy can have an impact
-      // (like functional components as one of the examples),
-      // you can manually specify Quasar components/directives to be available everywhere:
-      //
-      // components: [],
-      // directives: [],
-
-      // Quasar plugins
-      plugins: ['Notify']
+    return: {
+      framework: {
+        plugins: [
+          'Notify'
+        ],
+      }
     },
 
     // animations: 'all', // --- includes all animations
